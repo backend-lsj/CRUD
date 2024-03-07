f52c81f8d794b4ab41bb2f201c21713da020b2e7
lsj0202 <w1004123@gmail.com>
2024-03-07 20:12:00 +0900

    feat: add ip decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ip } from './decorators/ip.decotator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Ip() ip: string): string {
+    console.log(ip);
     return this.appService.getHello();
+  }
+
+  @Get('name')
+  getName(@Query('name') name: string): string {
+    return `${name} hello`;
   }
 }
